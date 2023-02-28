787d543c8a19e03f7cd76396ea6f65888313f870
khoibear <73540095+khoibear@users.noreply.github.com>
2023-02-28 12:40:59 -0800

    Added Menu, AboutUs, Footer, FindUs, Gallery

--- src/container/index.js
+++ src/container/index.js
@@ -1,21 +1,21 @@
-/* import AboutUs from './AboutUs/AboutUs';
+import AboutUs from './AboutUs/AboutUs';
 import Chef from './Chef/Chef';
 import FindUs from './FindUs/FindUs';
 import Footer from './Footer/Footer';
-import Gallery from './Gallery/Gallery'; */
+import Gallery from './Gallery/Gallery';
 import Header from './Header/Header';
-/* import Intro from './Intro/Intro';
+import Intro from './Intro/Intro';
 import Laurels from './Laurels/Laurels';
 import SpecialMenu from './Menu/SpecialMenu';
- */
+
 export {
-/*   AboutUs,
+  AboutUs,
   Chef,
   FindUs,
   Footer,
-  Gallery, */
+  Gallery,
   Header,
-/*   Intro,
+  Intro,
   Laurels,
-  SpecialMenu, */
+  SpecialMenu,
 };
